f28d562eb736ebd9de952963f2b68b469465624d
TikvaYosef <tikvayosef13@gmail.com>
2021-12-12 14:37:22 +0200

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        public ActionResult UserName()
+        {
+            ViewBag.Message = "Tikva";
+            return View();
+        }
+        public ActionResult user1()
+        {
+            ViewBag.UserObject = new { firstName = "Tikva",lastName = "Yosef",age = 25 };
+            //ViewBag.ObjectOfUsers = UserObject;
+            return View();
+        }
+        public ActionResult NamesOfUsers()
+        {
+            string[] namesOfUsers = new string[] {"Tikva","Yafit","Eden","Keren","Yosef"};
+            ViewBag.ListOfUsers = namesOfUsers;
+            return View();
+        }
+         public ActionResult users()
+        {
+            return View();
+        } 
+    }
+}
---
+++ Controllers/movieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class movieController : Controller
+    {
+        // GET: movie
+        public ActionResult Index()
+        {
+            ViewBag.Title = "movies";
+
+            ViewBag.Message = "Your application description page.";
+           
+
+            return View();
+        }
+
+        public ActionResult Movie()
+        {
+            string[] movies = new string[] { "shrek", "big mama","big mama2","norbit" };
+            ViewBag.ListOfMovies = movies;
+          
+
+            return View();
+        }
+    }
+}
